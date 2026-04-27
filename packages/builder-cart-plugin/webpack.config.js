15818fd1ffedaea310f6a0e43ec0b5b7167145d4
Steven Hall <Steven.hall@goldenhippo.com>
2026-04-27 14:47:51 -0500

    chore: fix webpack config

--- packages/builder-cart-plugin/webpack.config.js
+++ packages/builder-cart-plugin/webpack.config.js
@@ -69,6 +69,7 @@ module.exports = {
       'Access-Control-Allow-Private-Network': 'true',
       'Access-Control-Allow-Origin': '*',
     },
+    client: false,
   },
   performance: {
     maxAssetSize: 1000000,
