038cbfa3388ec46264c8f80b9a35c9680bea21af
Steven Hall <Steven.hall@goldenhippo.com>
2026-05-27 18:06:17 -0500

    chore: remove bg color on footer section

--- packages/builder-cart-schemas/src/section/footer.model.ts
+++ packages/builder-cart-schemas/src/section/footer.model.ts
@@ -8,22 +8,11 @@ export const createFooterModel = (editUrl: string): ModelShape => {
     displayName: 'Footer',
     helperText: 'CMS controlled global footer',
     contentTitleField: undefined,
-    fields: [
-      {
-        name: 'backgroundColor',
-        friendlyName: 'Background Color',
-        type: 'color',
-        defaultValue: 'transparent',
-        helperText: 'Select the background color for the footer',
-        defaultCollapsed: false,
-      },
-    ],
+    fields: [],
     editingUrlLogic: 'return `' + editUrl + '/builder-footer-section?builder.preview=true&builder.frameEditing=true`',
   };
 };
 
 export type BuilderFooterModelContent = BuilderContent & {
-  data: BuilderResponseBaseData & {
-    backgroundColor: string;
-  };
+  data: BuilderResponseBaseData
 };
