02f0c8d388f4f4133bd24d1de26ea86c3660e225
Vikas Bhavsar <vikas.bhavsar@goldenhippo.com>
2026-06-02 16:55:13 -0700

    chore: change to phase 4

--- packages/builder-cart-plugin/src/application/admin/model-sync.ts
+++ packages/builder-cart-plugin/src/application/admin/model-sync.ts
@@ -149,7 +149,7 @@ export const MODEL_DEFINITIONS: ModelDefinition[] = [
   {
     name: 'promotional-card',
     displayName: 'Promotional Card',
-    phase: 3,
+    phase: 4,
     dependencies: ['product'],
     getShape: (ids, editUrl) => BuilderHelper.promotionalCardModel(ids['product'], editUrl),
   },
